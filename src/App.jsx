a29a71d026d2bd018819954f6f32e276fe70b186
jmpowell95 <156863586+jmpowell95@users.noreply.github.com>
2025-06-03 09:03:51 -0400

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,1 +1,77 @@
-export default function App() { return <h1>Replace me with full App code</h1>; }
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { Home } from "./pages/Home";
+import { Course } from "./pages/Course";
+import { Lesson } from "./pages/Lesson";
+import { Leaderboard } from "./pages/Leaderboard";
+import { Login } from "./pages/Login";
+import { Signup } from "./pages/Signup";
+import { Profile } from "./pages/Profile";
+import { Certificates } from "./pages/Certificates";
+import { AdminDashboard } from "./pages/AdminDashboard";
+import { Header } from "./components/Header";
+import { UserProvider } from "./context/UserContext";
+import { PrivateRoute } from "./components/PrivateRoute";
+
+export default function App() {
+  return (
+    <UserProvider>
+      <Router>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<Signup />} />
+          <Route
+            path="/course/:language"
+            element={
+              <PrivateRoute>
+                <Course />
+              </PrivateRoute>
+            }
+          />
+          <Route
+            path="/lesson/:language/:id"
+            element={
+              <PrivateRoute>
+                <Lesson />
+              </PrivateRoute>
+            }
+          />
+          <Route
+            path="/leaderboard"
+            element={
+              <PrivateRoute>
+                <Leaderboard />
+              </PrivateRoute>
+            }
+          />
+          <Route
+            path="/profile"
+            element={
+              <PrivateRoute>
+                <Profile />
+              </PrivateRoute>
+            }
+          />
+          <Route
+            path="/certificates"
+            element={
+              <PrivateRoute>
+                <Certificates />
+              </PrivateRoute>
+            }
+          />
+          <Route
+            path="/admin"
+            element={
+              <PrivateRoute>
+                <AdminDashboard />
+              </PrivateRoute>
+            }
+          />
+        </Routes>
+      </Router>
+    </UserProvider>
+  );
+}
